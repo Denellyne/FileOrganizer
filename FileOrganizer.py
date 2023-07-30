ddbbb29cc2b7b1d9cc490d29f177d6a355cde5d3
Denellyne <56112881+Denellyne@users.noreply.github.com>
2023-07-30 20:42:19 +0100

    Add files via upload

---
+++ FileOrganizer.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+def ChangeFolders(extension,extensionFolder):
+    source = "C:\\Users\\{UserName}\\Downloads"
+    files = [x for x in os.listdir(source) if x.endswith(extension)]
+
+    destination = "C:\\Users\\{UserName}\\Downloads\\{}".format(extensionFolder)
+
+    for file in files:
+        src_path = os.path.join(source, file)
+        dst_path = os.path.join(destination , file)
+        os.rename (src_path , dst_path)
+
+i = 0
+
+while (i != 1):
+    for j in range(6):
+        match j:
+            case 1:
+                ChangeFolders(extension=".rar",extensionFolder="Rar")
+            case 2:
+                ChangeFolders(extension=".exe",extensionFolder="Exe")
+            case 3:
+                ChangeFolders(extension=".zip",extensionFolder="Rar")
+            case 4:
+                ChangeFolders(extension=".pdf",extensionFolder="Documents")
+            case 5:
+                ChangeFolders(extension=".docx",extensionFolder="Documents")
+            case 6:
+                ChangeFolders(extension=".txt",extensionFolder="Documents")
+    time.sleep(1800)
+    
+
+
