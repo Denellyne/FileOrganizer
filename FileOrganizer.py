998c3991e72ac69a8911bb6bd3e9ca7950ce46bf
Denellyne <56112881+Denellyne@users.noreply.github.com>
2023-09-14 08:21:42 +0100

    Bug Fixes
    Fixed a problem where the program wouldn't create the folder if it wasnt present

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,21 +1,54 @@
 import os
 import time
 
+i = 0
+
 def ChangeFolders(extension,extensionFolder):
-    source = "C:\\Users\\{UserName}\\Downloads"
+    source = "C:\\Users\\Gustavo Santos\\Downloads"
     files = [x for x in os.listdir(source) if x.endswith(extension)]
 
-    destination = "C:\\Users\\{UserName}\\Downloads\\{}".format(extensionFolder)
+    destination = "C:\\Users\\Gustavo Santos\\Downloads\\{}".format(extensionFolder)
 
     for file in files:
         src_path = os.path.join(source, file)
         dst_path = os.path.join(destination , file)
         os.rename (src_path , dst_path)
 
-i = 0
+
+def createFolder():
+    i = 0
+    userLogin = os.environ.get("USERNAME")
+    parentDirectory = "Downloads"
+    
+    while (i != 1):
+        for j in range(5):
+            match j:
+                case 1:
+                    folderName = "Rar"
+                    path = os.path.join("C:/users",userLogin,parentDirectory,folderName)
+                    if (os.path.isdir(path) != True):
+                        os.mkdir(path)
+                case 2:
+                    folderName = "Images"
+                    path = os.path.join("C:/users",userLogin,parentDirectory,folderName)
+                    if (os.path.isdir(path) != True):
+                        os.mkdir(path)
+                case 3:
+                    folderName = "Exe"
+                    path = os.path.join("C:/users",userLogin,parentDirectory,folderName)
+                    if (os.path.isdir(path) != True):
+                        os.mkdir(path)
+                case 4:
+                    folderName = "Documents"
+                    path = os.path.join("C:/users",userLogin,parentDirectory,folderName)
+                    if (os.path.isdir(path) != True):
+                        os.mkdir(path)
+    
+    
+createFolder()
 
 while (i != 1):
-    for j in range(6):
+    for j in range(15):
         match j:
             case 1:
                 ChangeFolders(extension=".rar",extensionFolder="Rar")
@@ -29,7 +62,17 @@ while (i != 1):
                 ChangeFolders(extension=".docx",extensionFolder="Documents")
             case 6:
                 ChangeFolders(extension=".txt",extensionFolder="Documents")
+            case 7:
+                ChangeFolders(extension=".7z",extensionFolder="Rar")
+            case 8:
+                ChangeFolders(extension=".zip",extensionFolder="Rar")
+            case 9:
+                ChangeFolders(extension=".png",extensionFolder="Images")
+            case 10:
+                ChangeFolders(extension=".jpg",extensionFolder="Images")
+            case 11:
+                ChangeFolders(extension=".jpeg",extensionFolder="Images")
     time.sleep(1800)
     
 
-
+    
